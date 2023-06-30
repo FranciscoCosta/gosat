1a5d151a4b7698d3ae37846b232a099cbce0d87b
Francisco Costa <francisco100eg@gmail.com>
2023-06-30 08:33:45 -0300

    Feat: Added offers to database if offer already exists skips it

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Offer;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -10,17 +11,40 @@ class OfferController extends Controller
     public function index(Request $request)
     {
         $cpf = $request->input('cpf');
+
         $response = Http::post('https://dev.gosat.org/api/v1/simulacao/credito', [
             'cpf' => $cpf
         ]);
-        return response()->json(['institution' => $response], 200);
-    }
-    // {
-    // [
-    //     'cpf' => $cpf,
-    // ]);
-    // return $response->json();
-    //     dd($response->json());
-    // }
+        $institutions = $response->json()['instituicoes'];
 
+        foreach ($institutions as $institution) {
+            $institutionId = $institution['id'];
+            $institutionName = $institution['nome'];
+            $modalities = $institution['modalidades'];
+
+            foreach ($modalities as $modality) {
+                $nameModal = $modality['nome'];
+                $cod = $modality['cod'];
+
+                $existingOffer = Offer::where([
+                    'cpf' => $cpf,
+                    'institution_id' => $institutionId,
+                    'name_modal' => $nameModal,
+                    'cod' => $cod,
+                ])->first();
+
+                if (!$existingOffer) {
+                    Offer::create([
+                        'cpf' => $cpf,
+                        'institution' => $institutionName,
+                        'institution_id' => $institutionId,
+                        'name_modal' => $nameModal,
+                        'cod' => $cod,
+                    ]);
+                }
+            }
+        }
+
+        return response()->json(['offers' => $institutions], 200);
+    }
 }
